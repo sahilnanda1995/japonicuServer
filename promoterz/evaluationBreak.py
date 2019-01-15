dbce7d813d852ab12db25cbbab3b9fb1e96d7ece
Sahil nanda <sahilnanda@saurabhs-MacBook-Air.local>
2019-01-15 15:27:06 +0530

    printing error

--- promoterz/evaluationBreak.py
+++ promoterz/evaluationBreak.py
@@ -122,7 +122,7 @@ def showResults(World):
             		currentSessionBreakResults.append((backtestResult['relativeProfit'],
                                                TOMLSettings))
             except Exception as e:
-            		World.logger.log(e)
+            		print e.message
             		pass		
             paramsFilename = "%s-EPOCH%i" % (LOCALE.name,
                                              LOCALE.EPOCH)
